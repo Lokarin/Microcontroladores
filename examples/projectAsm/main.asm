c51af7121cb136ac1b878eeaefd36a92297a2e75
Lokarin <onukiamaral@protonmail.com>
2025-04-04 10:34:25 -0300

    Exemplo de assembly

---
+++ examples/projectAsm/main.asm
@@ -0,0 +1,184 @@
+; ------------------------------------------------------------------------------
+; Project:  LED Blink
+; File:     ledBlink.asm
+; Author:   Leandro Schwarz
+; Created:  2024-09-23
+; Modified: 2024-09-23
+; Version:  1.0
+; Notes:    Blinks a LED. Fcpu = 16 MHz.
+; ------------------------------------------------------------------------------
+
+; ------------------------------------------------------------------------------
+; Include definition files
+; ------------------------------------------------------------------------------
+.include "include/m328Pdef.inc"
+
+; ------------------------------------------------------------------------------
+; Register definitions
+; ------------------------------------------------------------------------------
+.def    auxReg       = R16
+
+; ------------------------------------------------------------------------------
+; Interrupt vectors
+; ------------------------------------------------------------------------------
+.org 0x0000
+    JMP resetHandler
+.org INT0addr
+    JMP int0Handler
+.org INT1addr
+    JMP int1Handler
+.org PCI0addr
+    JMP pcint0Handler
+.org PCI1addr
+    JMP pcint1Handler
+.org PCI2addr
+    JMP pcint2Handler
+.org WDTaddr
+    JMP wdtHandler
+.org OC2Aaddr
+    JMP timer2CompAHandler
+.org OC2Baddr
+    JMP timer2CompBHandler
+.org OVF2addr
+    JMP timer2OvfHandler
+.org ICP1addr
+    JMP timer1CaptureHandler
+.org OC1Aaddr
+    JMP timer1CompAHandler
+.org OC1Baddr
+    JMP timer1CompBHandler
+.org OVF1addr
+    JMP timer1OvfHandler
+.org OC0Aaddr
+    JMP timer0CompAHandler
+.org OC0Baddr
+    JMP timer0CompBHandler
+.org OVF0addr
+    JMP timer0OvfHandler
+.org SPIaddr
+    JMP spiHandler
+.org URXCaddr
+    JMP usartRxHandler
+.org UDREaddr
+    JMP usartDataEmptyHandler
+.org UTXCaddr
+    JMP usartTxHandler
+.org ADCCaddr
+    JMP adcHandler
+.org ERDYaddr
+    JMP eepromHandler
+.org ACIaddr
+    JMP analogCompHandler
+.org TWIaddr
+    JMP twiHandler
+.org SPMRaddr
+    JMP flashHandler
+.org INT_VECTORS_SIZE
+
+; ------------------------------------------------------------------------------
+; Include other assembly files
+; ------------------------------------------------------------------------------
+; NONE
+
+; ------------------------------------------------------------------------------
+; Constants stored in Flash memory
+; Note: Variables must be multiples of 2, since memory is organized in 16 bits
+; ------------------------------------------------------------------------------
+; NONE
+
+; ------------------------------------------------------------------------------
+; Main function
+; ------------------------------------------------------------------------------
+main:
+    SBI     DDRB, PB5
+mainLoop:
+    SBI     PORTB, PB5
+    RCALL   delay100
+    CBI     PORTB, PB5
+    RCALL   delay100
+    JMP     mainLoop
+
+; ------------------------------------------------------------------------------
+; Function definitions
+; ------------------------------------------------------------------------------
+delay100:
+    NOP                     ; Comment line for CALL / Uncomment for RCALL
+    LDI     R18, 9
+    LDI     R19, 30
+    LDI     R20, 226
+delay100Loop:
+    DEC     R20
+    BRNE    delay100Loop
+    DEC     R19
+    BRNE    delay100Loop
+    DEC     R18
+    BRNE    delay100Loop
+    RJMP    PC + 1
+    RET
+
+delay250:
+    NOP                     ; Comment line for CALL / Uncomment for RCALL
+    LDI     R18, 21
+    LDI     R19, 75
+    LDI     R20, 188
+delay250Loop:
+    DEC     R20
+    BRNE    delay250Loop
+    DEC     R19
+    BRNE    delay250Loop
+    DEC     R18
+    BRNE    delay250Loop
+    RJMP    PC + 1
+    RET
+
+delay500:
+    NOP                     ; Comment line for CALL / Uncomment for RCALL
+    LDI     R18, 41
+    LDI     R19, 150
+    LDI     R20, 125
+delay500Loop:
+    DEC     R20
+    BRNE    delay500Loop
+    DEC     R19
+    BRNE    delay500Loop
+    DEC     R18
+    BRNE    delay500Loop
+    NOP
+    RET
+
+; ------------------------------------------------------------------------------
+; Interrupt handlers
+; ------------------------------------------------------------------------------
+int0Handler:
+int1Handler:
+pcint0Handler:
+pcint1Handler:
+pcint2Handler:
+wdtHandler:
+timer2CompAHandler:
+timer2CompBHandler:
+timer2OvfHandler:
+timer1CaptureHandler:
+timer1CompAHandler:
+timer1CompBHandler:
+timer1OvfHandler:
+timer0CompAHandler:
+timer0CompBHandler:
+timer0OvfHandler:
+spiHandler:
+usartRxHandler:
+usartDataEmptyHandler:
+usartTxHandler:
+adcHandler:
+eepromHandler:
+analogCompHandler:
+twiHandler:
+flashHandler:
+resetHandler:
+    LDI     R16, 0
+    STS     UCSR0B, R16
+    LDI     R19, LOW(RAMEND)
+    OUT     SPL, R19
+    LDI     R19, HIGH(RAMEND)
+    OUT     SPH,R19
+    JMP     main
