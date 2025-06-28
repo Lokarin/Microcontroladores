90a438a2504e2b8cf6154f649e493f86001aa922
Lokarin <onukiamaral@protonmail.com>
2025-06-28 20:59:31 -0300

    Novo teste de SPI

---
+++ c/playground/spiCommunication2/receptor/main.cpp
@@ -0,0 +1,69 @@
+#define F_CPU 16000000UL
+
+#include "funsape/funsapeLibGlobalDefines.hpp"
+#include "funsape/peripheral/funsapeLibInt0.hpp"
+#include "funsape/device/funsapeLibHd44780.hpp"
+#include "spiAtmega328p/spiAtmega328p.hpp"
+
+Hd44780 display;
+vuint8_t spiReceived = 0;
+volatile bool_t dataReady = false;
+
+int main()
+{
+    // Variables
+    GpioPin gpioDisplayEn;
+    GpioPin gpioDisplayRs;
+    GpioPin gpioDisplayRw;
+    GpioBus gpioDisplayData;
+
+    // Configure SPI
+    Spi::init(Spi::Mode::MASTER, Spi::ClockRate::FOSC_4);
+
+    // Configure INT0
+    int0.init(Int0::SenseMode::FALLING_EDGE);
+    int0.activateInterrupt();
+
+    // Configure Gpio Pins
+    gpioDisplayEn.init(&DDRD, GpioPin::PinIndex::P0);
+    gpioDisplayRs.init(&DDRD, GpioPin::PinIndex::P1);
+    gpioDisplayRw.init(&DDRB, GpioPin::PinIndex::P0);
+    gpioDisplayData.init(&DDRD, GpioBus::PinIndex::P3, 4);
+
+    // Configure Display
+    display.setControlPort(
+            &gpioDisplayEn, 
+            &gpioDisplayRs,
+            &gpioDisplayRw
+    );
+    display.setDataPort(&gpioDisplayData);
+    display.init(Hd44780::Size::LCD_16X2);
+    display.stdio();
+    printf("--Teste do LCD--\n0123456789ABCDEF\n");
+    delayMs(2000);
+    display.clearScreen();
+
+    // SS do Slave
+    setBit(DDRB, PB1);
+    clrBit(PORTB, PB1);
+
+    sei();
+
+    while(true) {
+        if (dataReady) {
+            uint16_t dataFull = spiReceived << 2;
+            display.clearScreen();
+            printf("ADC: %u", dataFull);
+            dataReady = false;
+        }
+        delayMs(300);
+    }
+
+    return 0;
+}
+
+void int0InterruptCallback() {
+    Spi::sendByte(0);
+    spiReceived = Spi::getLastReceived();
+    dataReady = true;
+}
---
+++ c/playground/spiCommunication2/transmissor/main.cpp
@@ -0,0 +1,59 @@
+#define F_CPU 16000000UL
+
+#include "funsape/funsapeLibGlobalDefines.hpp"
+#include "funsape/peripheral/funsapeLibAdc.hpp"
+#include "funsape/peripheral/funsapeLibTimer0.hpp"
+#include "spiAtmega328p/spiAtmega328p.hpp"
+
+int main()
+{
+    // Configure SPI
+    Spi::init(Spi::Mode::SLAVE, Spi::ClockRate::FOSC_4);
+
+    // Configure ADC
+    adc.init(
+            Adc::Mode::AUTO_TIMER0_COMPA, 
+            Adc::Reference::INTERNAL, 
+            Adc::Prescaler::PRESCALER_128
+    );
+    adc.setChannel(Adc::Channel::CHANNEL_0);
+    adc.clearInterruptRequest();
+    adc.activateInterrupt();
+    adc.enable();
+
+    // Configure Timer0
+    timer0.setCompareAValue(156-1);
+    timer0.init(Timer0::Mode::CTC_OCRA, Timer0::ClockSource::PRESCALER_1024);
+    timer0.clearCompareAInterruptRequest();
+
+    // Debug
+    setBit(DDRC, PC1);
+
+    // Pino de inicio de comunicação
+    setBit(DDRD, PD0);
+    setBit(PORTD, PD0);
+
+    sei();
+
+    while(true) {
+    }
+
+    return 0;
+}
+
+void adcConversionCompleteCallback(void) {
+    timer0.clearCompareAInterruptRequest();
+    //cplBit(PORTC, PC1);
+    
+    uint16_t adcValue = ADC; // Ex: 0b11_11111111
+
+    // Enviando 8bits de 16bits compressos
+    SPDR = adcValue >> 2;     // Divisão por 4
+
+    // Avisar o Master
+    clrBit(PORTD, PD0);
+}
+
+void Spi::spiCallbackInterrupt(uint8_t received) {
+    setBit(PORTD, PD0);
+}
